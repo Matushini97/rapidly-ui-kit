348e5df8dfe4194a127be08671259ce36d46d620
Ilya Matyushin <IVMatyushin@alfabank.ru>
2025-03-09 20:03:01 +0300

    feat: make structure

---
+++ src/components/button/button.stories.tsx
@@ -0,0 +1,72 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button } from '.'
+
+
+const meta = {
+  argTypes: {
+    onClick: { action: 'clicked' },
+    variant: {
+      control: { type: 'radio' },
+      options: ['primary', 'secondary', 'outlined', 'link'],
+    },
+  },
+  component: Button,
+  tags: ['autodocs'],
+  title: 'Components/Button',
+} satisfies Meta<typeof Button>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    children: 'Primary Button',
+    disabled: false,
+    variant: 'primary',
+  },
+}
+
+export const Secondary: Story = {
+  args: {
+    children: 'Secondary Button',
+    disabled: false,
+    variant: 'secondary',
+  },
+}
+export const Outlined: Story = {
+  args: {
+    children: 'Outlined Button',
+    disabled: false,
+    variant: 'outlined',
+  },
+}
+export const Link: Story = {
+  args: {
+    children: 'Button that looks like a link',
+    disabled: false,
+    variant: 'link',
+  },
+}
+
+export const FullWidth: Story = {
+  args: {
+    children: 'Full Width Button',
+    disabled: false,
+    fullWidth: true,
+    variant: 'primary',
+  },
+}
+
+export const AsLink: Story = {
+  args: {
+    ...Primary.args,
+    children: 'Link',
+  },
+  render: args => {
+    return (
+      <Button {...args} asChild>
+        <a>Go to google</a>
+      </Button>
+    )
+  },
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
+import { dependencies, devDependencies, peerDependencies } from './package.json'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      // the proper extensions will be added
+      fileName: 'index',
+      formats: ['es'],
+      name: '@rapidly/ui-kit',
+    },
+    rollupOptions: {
+      external: [
+        ...Object.keys(peerDependencies),
+        ...Object.keys(dependencies),
+        ...Object.keys(devDependencies),
+        'react/jsx-runtime',
+      ],
+      output: {
+        dir: 'dist',
+        entryFileNames: '[name].js',
+        format: 'es',
+      },
+    },
+    sourcemap: true,
+    target: 'esnext',
+  },
   plugins: [react()],
 })
